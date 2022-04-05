74e4b5ee30fd7976976e055185e9ec1120adec14
Carlos Pizarro <carlospizarromorales@gmail.com>
2022-04-05 09:02:21 -0400

    corrige alto de .card-img.top[1] en toda circunstancia

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,39 @@
-// Codigo desafio prueba Viajes Chile
 // Habilita tooltips
 let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
-// Código para cambiar opacidad de navbar
-// al hacer scroll down 
-// let i = 0;
-let oldScroll = 0;
-let tictac = false;
+window.onload = function () {
+  // Código para cambiar opacidad de navbar al hacer scroll down 
+  let i = 0;
+  let oldScroll = 0;
+  let tictac = false;
 
-document.addEventListener('scroll', e => {
-  oldScroll = window.scrollY;
+  document.addEventListener('scroll', e => {
+    oldScroll = window.scrollY;
 
-  if (!tictac) {
-    window.requestAnimationFrame(() => {
-      // i = window.scrollY / 1000;
-      document.body.childNodes[5].style.backgroundColor = `rgba(23,163,187,${window.scrollY / 1000}`;
-      tictac = false;
-    });
+    if (!tictac) {
+      window.requestAnimationFrame(() => {
 
-    tictac = true;
-  }
-})
+        i = window.scrollY / 1000;
+        document.querySelector('.navbar').style.backgroundColor = `rgba(23,163,187,${i}`;
+        tictac = false;
+      });
 
+      tictac = true;
+    }
+  })
 
+  // funcion que ajusta el tamaño de la imagen 2 a las otras 3, 
+  // sin importar el tamano del viewport
+  const ajustarImagen =  () => {
+    let imagenes = document.getElementsByClassName('card-img-top');
+    imagenes[1].style.height = window.getComputedStyle(imagenes[0]).height;
+    // imagenes.forEach(e => console.log(window.getComputedStyle(e).height));
+  };
+
+  ajustarImagen(); // al cargar
+  window.addEventListener('resize', ajustarImagen); // al ajustar la ventana del browser
+
+}
